ff75204e078280fa7dbb214d255bb0ae3295332f
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-04-02 21:01:36 -0400

    Updated for 1.4.1+ 	Added support for ClickThroughBlocker 	Added support for ToolbarController 	Added settings page for Blizzy option 	Added toggle for Blizzy on window 	Updated build files 	Added Jenkins config

--- deploy.bat
+++ deploy.bat
@@ -1,8 +1,20 @@
 
-set H=R:\KSP_1.3.1_dev
-echo %H%
+@echo off
 
+rem H is the destination game folder
+rem GAMEDIR is the name of the mod folder (usually the mod name)
+rem GAMEDATA is the name of the local GameData
+rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
+rem    but not always
 
-copy KerbalSlingshotter\KerbalSlingshotter\bin\%1\KerbalSlingshotter.dll GameData\SlingShotter\Plugins
-xcopy /E /Y /i GameData\Slingshotter %H%\GameData\Slingshotter
+set H=R:\KSP_1.4.1_dev
+set GAMEDIR=SlingShotter
+set GAMEDATA="GameData\"
+set VERSIONFILE=%GAMEDIR%.version
 
+copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
+
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
+
+pause
