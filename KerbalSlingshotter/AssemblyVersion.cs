236f9a32dff1808ad05b6dd964e5206b6d8fe16a
Jonathan Bayer <linuxgurugamer@gmail.com>
2018-10-28 05:43:29 -0400

    Version bump for 1.5 rebuild

--- KerbalSlingshotter/AssemblyVersion.cs
+++ KerbalSlingshotter/AssemblyVersion.cs
@@ -10,9 +10,9 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("1.1.6.7")]
+ [assembly: AssemblyVersion("1.1.6.8")]
   public partial class FMRS_Version_Info
   {
-   public const string beta_version = "7";
-   public const string version_number = "1.1.6.7";
+   public const string beta_version = "8";
+   public const string version_number = "1.1.6.8";
   }
